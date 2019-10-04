22ddd6b1a78120a9fa6d92a6921ecc9c3f7d99c6
Euan <ehollidge@hotmail.com>
2019-10-04 11:38:33 +0200

    Update install.bash

--- install.bash
+++ install.bash
@@ -2,8 +2,7 @@ OS=`uname -s`
 if [[ $OS == "Linux" ]]; then
 	apt-get update
 	apt-get install -y software-properties-common curl make gcc ggcov clang
-	apt-get install -y libx11-dev:i386 libx11-dev  xorg-dev libbsd libbsd-dev 
-	apt-get install -y libxext-dev
+	apt-get install -y lxorg-dev libxext-dev
 	add-apt-repository -y ppa:snaipewastaken/ppa
 	apt-get update
 	apt-get install -y criterion-dev
