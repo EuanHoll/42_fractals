0c789d38ac88024fe4f7a60a3f43bc7b4c1b6da1
Euan <ehollidge@hotmail.com>
2019-10-04 11:15:34 +0200

    Update install.bash

--- install.bash
+++ install.bash
@@ -2,7 +2,7 @@ OS=`uname -s`
 if [[ $OS == "Linux" ]]; then
 	apt-get update
 	apt-get install -y software-properties-common curl make gcc ggcov clang
-	apt-get install -y xorg libx11-dev xorg-dev
+	apt-get install -y libx11-dev xorg-dev
 fi
 
 make
